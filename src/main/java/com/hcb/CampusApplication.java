baf49d2bba89eff94dbfac1e609eba6fb61dda04
13376625680 <648068645@qq.com>
2023-07-30 23:17:01 +0800

    hxz

--- src/main/java/com/hcb/CampusApplication.java
+++ src/main/java/com/hcb/CampusApplication.java
@@ -18,6 +18,6 @@ public class CampusApplication {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();//。。。
     }
 }
